604713156f607c6174d5054a7bd46356e8952df0
victorfouquet <victor.fouquet@gmail.com>
2019-07-23 22:03:30 +0200

    cleaning app & app_2

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ require_relative 'lib/player'
 player1 = Player.new("Diego le Gitan")
 player2 = Player.new("Boris le Soviet")
 
-puts "Mesdames, Mesdemoiselles, Messieurs, bienvenue au combat du siècle !", ""
-puts "Dans l'arène à gauche, le célèbre, le redoutable, l'infâme, Diego le Gitaaaaannnn !!"
-puts "A droite l'outsider du jour, l'irresponsable, le brutal, le fauve sibérien, Boris le Sovieeeeet !!", ""
+puts "	Mesdames, Mesdemoiselles, Messieurs, bienvenue au combat du siècle !", ""
+puts "	Dans l'arène à gauche, le célèbre, le redoutable, l'infâme, Tony le Forrraaaiiinnn !!"
+puts "	A droite l'outsider du jour, l'irresponsable, le brutal, le fauve sibérien, Boris le Rooouuuuge !!", ""
 puts player1.show_state
 puts player2.show_state
-puts "", "Combattants, soyez sans pitié!", ""
+puts "", "	Combattants, soyez sans pitié!", ""
 
 while player1.life_points > 0 && player2.life_points > 0
 	player2.attacks(player1)
@@ -28,5 +28,3 @@ while player1.life_points > 0 && player2.life_points > 0
 	end
 	puts player2.show_state,""
 end
-
-#binding.pry
--- app_2.rb
+++ app_2.rb
@@ -4,25 +4,26 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 		 
-puts "	 ------------------------------------------------"
-puts "	|    Bienvenue sur 'Ils veulent tous ma POO' !   |"
-puts "	| Le but du jeu est d'être le dernier survivant! |"
-puts " 	------------------------------------------------"
-puts "", "Quel est le nom de ton personnage? "
+puts "	 	 ------------------------------------------------"
+puts "		|    Bienvenue sur 'Ils veulent tous ma POO' !   |"
+puts "		| Le but du jeu est d'être le dernier survivant! |"
+puts " 		 ------------------------------------------------"
+puts "", "	Quel est le nom de ton personnage? "
 print "> " 
 user = HumanPlayer.new(gets.chomp)
 
 enemies = [player1 = Player.new("Tony le Forain"), player2 = Player.new("Boris le Rouge")]
 
 while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
-	puts "", "	++++++++++++++++++++++++++++++++++++++++++++++++++"
+	sleep(2)
+	puts "", "		++++++++++++++++++++++++++++++++++++++++++++++++++"
 	puts "", user.show_state, ""
-	puts "Quelle action veux-tu effectuer ?", "", "a - chercher une meilleure arme", "s - chercher à se soigner", ""
-	puts "Attaquer un joueur en vue :", "0 - #{player1.show_state}", "1 - #{player2.show_state}", ""
+	puts "	Quelle action veux-tu effectuer ?", "", "		a - chercher une meilleure arme", "		s - chercher à se soigner", ""
+	puts "	Attaquer un joueur en vue :", "		0 - #{player1.show_state}", "		1 - #{player2.show_state}", ""
 	print "> " 
 	choice = gets.chomp
 	while !["a", "s", "0", "1"].include? choice
-		puts "Merci d'entrer un choix conforme aux propositions si tu veux que ça marche ;)"
+		puts "	Merci d'entrer un choix conforme aux propositions si tu veux que ça marche ;)"
 		print "> "
 		choice = gets.chomp
 	end
@@ -42,10 +43,10 @@ while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 
 	end
 end
 
-puts "", "	++++++++++++++++++++++++++++++++++++++++++++++++++"
-puts "", "La partie est finie."
+puts "", "		++++++++++++++++++++++++++++++++++++++++++++++++++"
+puts "", "	La partie est finie.", ""
 if user.life_points > 0
-	puts "B R A V O !  T U  A S  G A G N E !! "
+	puts "		       B R A V O !  T U  A S  G A G N E !! "
 else
-	puts "Loser ! Tu as perdu !"
+	puts "	Loser ! Tu as perdu !"
 end
